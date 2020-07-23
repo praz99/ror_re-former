a804a6b5bb7551bb04ef1e2959b65e9000adf670
Okikiola Apelehin <okikiola.apelehin@gmail.com>
2020-07-23 11:20:25 +0000

    Add the edit controller and view Add the update controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new()
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to user_path(@user)
   end
 
   def create
